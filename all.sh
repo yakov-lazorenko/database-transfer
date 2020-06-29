a7e554a585b572539ff194daaefa4be9b5d58378
yakov-lazorenko <yakov.lazorenko@gmail.com>
2020-06-29 22:04:21 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -3,15 +3,9 @@
 # this sсript transfers data from old DB to new DB
 # DB configs located in file 'bootstrap.php'
 
-php transfer-archive-articles.php ;
-
-php transfer-archive-photos.php ;
-
 php transfer-articles.php ;
 
 php transfer-article-tag.php ;
-
-php transfer-photo-articles.php ;
 
 php transfer-categories.php ;
 
@@ -19,11 +13,7 @@ php transfer-tags.php ;
 
 php transfer-users.php ;
 
-php transfer-contests.php ;
-
 php transfer-galleries.php ;
-
-php transfer-special-themes.php ;
 
 php transfer-static-pages.php ;
 
